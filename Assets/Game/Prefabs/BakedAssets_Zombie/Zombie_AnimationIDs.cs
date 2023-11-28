b940e16b0a32a8b5475383fcc00c5e46221503b3
BarisTasin <tasinbaris95@gmail.com>
2023-11-28 16:53:58 +0300

    New zombie animations added

--- Assets/Game/Prefabs/BakedAssets_Zombie/Zombie_AnimationIDs.cs
+++ Assets/Game/Prefabs/BakedAssets_Zombie/Zombie_AnimationIDs.cs
@@ -3,8 +3,11 @@ namespace GPUECSAnimationBaker.Engine.AnimatorSystem
     public enum AnimationIdsZombieMovement
     {
         ZombieMovement = 0,
-        Dying1 = 1,
-        Dying2 = 2,
-        Dying3 = 3
+        ZombieMovement1 = 1,
+        ZombieMovement2 = 2,
+        Dying1 = 3,
+        Dying2 = 4,
+        Dying3 = 5,
+        Dying4 = 6
     }
 }
